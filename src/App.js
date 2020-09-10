8b9bbf1d1e6cef88bf57f6afb6d639a09c09a646
Marlen DESAIGUES <Marlen@MacBook-Air-de-Marlen.local>
2020-09-10 12:29:20 +0200

    cours2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import LandingPage from './LandingPage.js'
+import React, { useState } from 'react'
+import Button from './components/Button'
+import Displayer from './components/Displayer'
 
-function App() {
-  return (
-    <div>
-      <LandingPage />
+const App = (props) => {
+  const [title, setTitle] = useState()
+  const [message, setMessage] = useState()
+
+  const discuss = [
+    {
+        username: "Bob",
+        content: "Salut",
+        type: "A"
+    },
+    {
+        username: "Alice",
+        content: "Salut ca va ?",
+        type: "B"
+    }
+  ]
+
+  return(
+    <div className="app-container">
+      <h1>Messagerie</h1>
+      <Displayer messages={discuss} />
     </div>
-  );
+  )
 }
 
-export default App;
+const Form = (props) => {
+  const [content, setContent] = useState()
+  return(
+    <form>
+      <input type="text" value={content} onChange={(event) => {
+        setContent(event.target.value)
+      }} />
+    </form>
+  )
+}
+
+export default App
