b396a5178eabd5b55d68ca784e4987fef7af06d5
Marlen DESAIGUES <Marlen@MacBook-Air-de-Marlen.local>
2020-09-11 16:16:54 +0200

    cours3

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import Header from './components/Header.js'
+import Navbar from './components/Navbar.js'
+import Title from './components/Title.js'
+import Skill from './components/Skill.js'
+import { skills } from './config'
 
 const LandingPage = () => {
     return <div>
+        <Navbar />
         <Header 
-            height="100vh"
-            color="blue"
-            title="Marlen DESAIGUES" />
-        <Header 
-            height="20vh"
-            color="red"
-            title="Contact" />
+            title="Mon site" 
+            description="Apprenez-en plus sur moi" 
+            buttonText="Me contacter"
+            imageUrl="https://wallpapercave.com/wp/TSlZ57J.jpg"
+        />
+        <div className="container">
+            <Title title="Mes compétences" description="Voici ce que je sais faire" />
+            {skills.map(item => {
+                return <Skill skill={item} />
+            })}
+        </div>
+            
     </div>
 }
 
